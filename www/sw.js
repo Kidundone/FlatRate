006757320db362113fc5d3ba4cb8de4574f99346
nelly boi <eamnelsonmalloy@icloud.com>
2026-06-30 16:08:07 -0400

    Fix theme legibility: --muted2 visible in dark mode, tour avatar in light mode

--- www/sw.js
+++ www/sw.js
@@ -8,7 +8,7 @@ const PAGES_CACHE  = "fr-pages-v1";
 // Hashed asset URLs for this build
 const HASHED_ASSETS = [
   "./app.8542b52fae.js",
-  "./app.d8371b24d8.css",
+  "./app.3edd8f9e4c.css",
 ];
 
 // Static assets cached for offline use (icons, manifest)
