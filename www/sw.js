77d99f567cf3fbd89f84bda880b2de88e28e4e78
nelly boi <eamnelsonmalloy@icloud.com>
2026-09-19 22:05:49 -0400

    build: versioned bundle (hour-chip overflow fix)

--- www/sw.js
+++ www/sw.js
@@ -8,7 +8,7 @@ const PAGES_CACHE  = "fr-pages-v1";
 // Hashed asset URLs for this build
 const HASHED_ASSETS = [
   "./app.0c963536c2.js",
-  "./app.58d2e26a54.css",
+  "./app.db5b182c4f.css",
 ];
 
 // Static assets cached for offline use (icons, manifest)
