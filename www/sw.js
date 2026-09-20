31119c0e8e8d640ff239f26813e7041c64ceaa25
nelly boi <eamnelsonmalloy@icloud.com>
2026-09-20 01:14:18 -0400

    build: versioned bundle (photo-viewer stacking fix)

--- www/sw.js
+++ www/sw.js
@@ -7,7 +7,7 @@ const PAGES_CACHE  = "fr-pages-v1";
 
 // Hashed asset URLs for this build
 const HASHED_ASSETS = [
-  "./app.e2c50babb6.js",
+  "./app.5858ccc401.js",
   "./app.db5b182c4f.css",
 ];
 
