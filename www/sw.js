8314f48445371b31adbf26a2354369880b448c42
nelly boi <eamnelsonmalloy@icloud.com>
2026-09-19 21:18:48 -0400

    build: versioned bundle (time-input width fix)

--- www/sw.js
+++ www/sw.js
@@ -8,7 +8,7 @@ const PAGES_CACHE  = "fr-pages-v1";
 // Hashed asset URLs for this build
 const HASHED_ASSETS = [
   "./app.0c963536c2.js",
-  "./app.828e323b1e.css",
+  "./app.a21a2ac64a.css",
 ];
 
 // Static assets cached for offline use (icons, manifest)
